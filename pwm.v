60f7b7005104d68e3eb7d756a733dfd3a1e0699f
Akhil Kapadia <akhil.kapadia@ttu.edu>
2020-02-04 20:25:02 -0600

    Added Pulse Wave Modulation code v1.0

---
+++ pwm.v
@@ -0,0 +1,10 @@
+'timescale 1ns/1ps
+
+module pwm(input clk, input [7:0] duty, output reg pulse);
+	reg [7:0] count =0;
+	always @(posedge clk)
+	begin
+		count <= count + 1;
+		pulse <= (count < duty);
+	end
+endmodule
