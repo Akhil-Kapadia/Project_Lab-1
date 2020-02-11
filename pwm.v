2e067c7843a2fa187b42ef2c46f97efdebf4c302
Akhil Kapadia <akhil.kapadia@ttu.edu>
2020-02-11 15:22:11 -0600

    Fixed the pwm switch problem. Should work now. Used shift and 12 bits...

--- pwm.v
+++ pwm.v
@@ -1,5 +1,5 @@
-module pwm(input clk, input [7:0] duty, output reg pulse);
-	reg [13:0] count =0;
+module pwm(input clk, input [11:0] duty, output reg pulse);
+	reg [11:0] count =0;
 	always@(posedge clk)
 	begin
 		count <= (count < 0xF<<8) ? count + 1 : 0;
--- switches.v
+++ switches.v
@@ -7,7 +7,7 @@ input [7:0] sw,		//Switch states
 output [3:0] IN, 		//Sets the inputs of HBRidge -> Controls direction  of rover.
 output [1:0] EN,
 input [1:0] OC,
-output [13:0] duty,	//For use in case statement in pwm.v -  Will store how many switches toggled.
+output [11:0] duty,	//For use in case statement in pwm.v -  Will store how many switches toggled.
 input pulse
 );
 reg temp[3:0];	//Saves old IN bits.
@@ -17,7 +17,7 @@ assign EN[1:0] = (|OC[1:0] && ~sw[0] ) ? 0 : pulse ; 				//Sets the EN to pulse 
 
 //Change the duty depending on how many switches toggled.
 
-assign duty = sw[7:4] << 10;
+assign duty = sw[7:4] << 8;
 000000001111 11110000000000
 
 //IN[0] == H-Bridge Input 1.
@@ -25,7 +25,7 @@ assign duty = sw[7:4] << 10;
 //IN[2] == H-Bridge Input 3.
 //IN[3] == H-Bridge Input 4.
 
-assign IN[7:4] = (sw[1] && ~sw[2] && ~sw[3]) ? 3'b0110 : 			//Sets rover forwards or backwards
+assign IN[7:4] = (sw[3:1] == 3'b001) ? 3'b0110 : 			//Sets rover forwards or backwards
 				 (sw[2] && ~sw[3]) ? 3'b0101 : 						//Sets rover to go right if ON
 				 (sw[3]) ? 3'b1010 : 3'b1001 ;						//Sets rover to go left if ON
 
