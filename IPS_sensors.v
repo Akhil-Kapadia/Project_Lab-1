129eba8c90efe86ea7be55d4437a8982309cba07
Akhil Kapadia <akhil.kapadia@ttu.edu>
2020-03-08 19:13:13 -0500

    Added latch to handle loss of IPS signals.

--- IPS_sensors.v
+++ IPS_sensors.v
@@ -11,11 +11,17 @@ reg [31:0] count=32'b0;
 reg [1:0] seconds = 4'b0;
 
 localparam max =200_000_000;
+reg [3:0] IN_Last;
 
 assign IN[3:0] = (~IPS[1]) ? 4'b1001 : 				//If middle IPS lit, Go forwards.
 				 (~IPS[2]) ? 4'b1010 :				//If left IPS lit, Go left.
 				 (~IPS[0]) ? 4'b0101 : 4'b0000;				//If right IPS lit, go right. If none lit stop.
 //				 (find_path) ? 4'b1010 : 4'b0101;	//If path lost turn left 90 degrees, if still not found turn right 180.
+
+//Latch to retain last value.
+always @ (posedge clk)
+	IN_Last <= IN;
+	
 
 //If path is lost, start turning left for 2 seconds (~90), if still not found turn right untill it is.
 //always @ (posedge clk)
